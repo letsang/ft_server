c3b44606b46ebdf7fbdf61ed4dcf68e70b2db108
jtsang <jtsang@student.42.fr>
2020-02-10 09:24:09 +0100

    ok ft_server just need to manage SSL on localhost

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,15 +1,15 @@
 service mysql start
 
 # CREATE WEBSITE FOLDER
-mkdir /var/www/ft_server
+mkdir /var/www/localhost
 
 # SET ACCESS
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
 
 # SET NGINX
-mv ./tmp/nginx-conf /etc/nginx/sites-available/ft_server
-ln -s /etc/nginx/sites-available/ft_server /etc/nginx/sites-enabled/ft_server
+mv ./tmp/nginx-conf /etc/nginx/sites-available/localhost
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
 rm -rf /etc/nginx/sites-enabled/default
 
 # SET MYSQL
@@ -19,29 +19,28 @@ echo "update mysql.user set plugin='mysql_native_password' where user='root';" |
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
 # DL phpmyadmin
-mkdir /var/www/ft_server/phpmyadmin
+mkdir /var/www/localhost/phpmyadmin
 cd /tmp/
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/ft_server/phpmyadmin
-mv /tmp/phpmyadmin.inc.php /var/www/ft_server/phpmyadmin/config.inc.php
+tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/localhost/phpmyadmin
+mv /tmp/phpmyadmin.inc.php /var/www/localhost/phpmyadmin/config.inc.php
 
 # DL wordpress
 cd /tmp/
 wget -c https://wordpress.org/latest.tar.gz
 tar -xvzf latest.tar.gz
-mv wordpress/ /var/www/ft_server
-mv /tmp/wp-config.php /var/www/ft_server/wordpress
+mv wordpress/ /var/www/localhost
+mv /tmp/wp-config.php /var/www/localhost/wordpress
 
 # SET SSL
 mkdir /etc/nginx/ssl
-#openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/ft_server.pem -keyout /etc/nginx/ssl/ft_server.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=jtsang/CN=ft_server"
 mkdir /root//mkcert
 cd /root/mkcert
 wget https://github.com/FiloSottile/mkcert/releases/download/v1.1.2/mkcert-v1.1.2-linux-amd64
 mv mkcert-v1.1.2-linux-amd64 mkcert
 chmod +x mkcert
 ./mkcert -install
-./mkcert ft_server
+./mkcert localhost
 cp *.pem /etc/nginx/ssl
 
 service php7.3-fpm start
